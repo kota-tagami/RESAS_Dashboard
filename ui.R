37ed1ed4b4772ed46b37ef1af73fca2bbfad11b0
KotaTagami <kota.tagami.123@gmail.com>
2020-10-20 00:02:13 +0900

    Moduling server and ui

--- ui.R
+++ ui.R
@@ -1,67 +1,67 @@
-select_pref <- function(id, label){
-  selectizeInput(
-    id, label,
-    width = "100%",
-    choices = prefs$prefName,
-    selected = default_pref
-  )
-}
-select_city <- function(id, label){
-  selectInput(
-    id, label,
-    width = "100%",
-    choices = default_cities,
-    selected = head(default_cities, 1)
-  )
-}
+##  title: UI
 
-set_button <- function(id, i, icon_name){
-  lab <- paste0("Set to Tab #", as.character(i))
-  actionButton(id, lab, icon = icon(icon_name), width = "100%")
-}
+##================================================##
+##>>  Section 1: UI  <<##
+##================================================##
 
+##++++++++++++++++++++++++++++++++++++++++++++++++##
+##>>  Sub-section 1: Surface  <<##
+##++++++++++++++++++++++++++++++++++++++++++++++++##
+ui_surface_input <- tagList(
+  fluidPage(
+    add_busy_bar(color = "#FF0000"),
+    wellPanel(
+      fluidRow(
+        column(6, select_pref("pref", "Prefecture")),
+        column(6, select_city("city", "City"))
+      ),
+      fluidRow(
+        column(3, set_button("triger1", 1, "cat")),
+        column(3, set_button("triger2", 2, "dog")),
+        column(3, set_button("triger3", 3, "crow")),
+        column(3, set_button("triger4", 4, "fish"))
+      )
+    )
+  )
+)
+
+ui_surface_output <- tagList(
+  tabsetPanel(
+    id = "outputTabset",
+    tabPanel("#1", icon = icon("cat"), outputUI("tab1")),
+    tabPanel("#2", icon = icon("dog"), outputUI("tab2")),
+    tabPanel("#3", icon = icon("crow"), outputUI("tab3")),
+    tabPanel("#4", icon = icon("fish"), outputUI("tab4"))
+  )
+)
+
+##++++++++++++++++++++++++++++++++++++++++++++++++##
+##>>  Sub-section 2: Depth  <<##
+##++++++++++++++++++++++++++++++++++++++++++++++++##
+
+
+##++++++++++++++++++++++++++++++++++++++++++++++++##
+##>>  Sub-section 3: Combined  <<##
+##++++++++++++++++++++++++++++++++++++++++++++++++##
 ui <- navbarPage(
   "RESAS Dashboard",
   theme = shinytheme("darkly"),
   collapsible = T,
   
+  #<<  Surface section  >>#
   tabPanel(
     "Surface",
-    
-    titlePanel("Dashboard Surface"),
-    
-    fluidPage(
-      add_busy_bar(color = "#FF0000"),
-      wellPanel(
-        fluidRow(
-          column(6, select_pref("pref", "Prefecture")),
-          column(6, select_city("city", "City"))
-        ),
-        
-        fluidRow(
-          column(3, set_button("triger1", 1, "cat")),
-          column(3, set_button("triger2", 2, "dog")),
-          column(3, set_button("triger3", 3, "crow")),
-          column(3, set_button("triger4", 4, "fish"))
-        )
-        
-      )
-    ),
-    
-    tabsetPanel(
-      tabPanel("#1", uiOutput("outputSection"), icon = icon("cat")),
-      tabPanel("#2", h1(icon("dog")), icon = icon("dog")),
-      tabPanel("#3", h1(icon("crow")), icon = icon("crow")),
-      tabPanel("#4", h1(icon("fish")), icon = icon("fish"))
-    ),
-
-
+    titlePanel("Look at the Surface"),
+    ui_surface_input,
+    ui_surface_output
   ),
   
-  
-  tabPanel("Depth", titlePanel("Dashboard Depth"))
-  
+  #<<  Depth section  >>#
+  tabPanel(
+    "Depth", 
+    titlePanel("Dive into the Depth"), 
+    h2("Coming soon")
+  )
 )
   
-  
-  
+##>>--------------END of Section 1--------------<<##
